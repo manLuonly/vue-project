d6b09c310ddfc617c20d4575ec9ddbabbc52efb1
luguangzong <1454118276@qq.com>
2018-12-12 21:08:10 +0800

    '完成轮播图'

--- fore-end/src/components/NavBar/index.vue
+++ fore-end/src/components/NavBar/index.vue
@@ -1,14 +1,101 @@
 <template>
   <ul class="nav-bar">
-    <h1>1</h1>
+    <router-link
+    v-for="(item, index) in bars"
+    :key="index"
+    :class="item.id"
+    :to="{ path: item.path }"
+    :tag="li"
+    active-class="z-act"
+    >
+    <i class="img"></i>
+    <span v-text="item.name"></span>
+    </router-link>
   </ul>
 </template>
 
 <script>
-  export default {
-    name: 'NavBar'
+export default {
+  name: 'NavBar',
+
+  data () {
+    return {
+      bars: [
+        {
+          id: 'films',
+          path: '/films/nowPlaying',
+          name: '电影'
+        },
+        {
+          id: 'cinemas',
+          path: 'cinemas',
+          name: '影院'
+        },
+        {
+          id: 'center',
+          path: 'center',
+          name: '我的'
+        }
+      ]
+    }
   }
+}
 </script>
 <style lang="scss" scoped>
+  @import '@/styles/common/px2rem.scss';
 
+.nav-bar {
+  flex-shrink: 0;
+  display: flex;
+  height: px2rem(50);
+  border-top: px2rem(1) solid #ededed;
+  img {
+    width: px2rem(21);
+    height: px2rem(21);
+  }
+  li {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: px2rem(12);
+
+    .img {
+      display: block;
+      width: px2rem(22);
+      height: px2rem(22);
+      background-size: 100%;
+    }
+
+    &.films {
+      .img {
+        background-image: url('./images/films.png');
+      }
+      &.z-act .img {
+        background-image: url('./images/films_on.png');
+      }
+    }
+    &.cinemas {
+      .img {
+        background-image: url('./images/cinemas.png');
+      }
+      &.z-act .img {
+        background-image: url('./images/cinemas_on.png');
+      }
+    }
+    &.center {
+      .img {
+        background-image: url('./images/center.png');
+      }
+      &.z-act .img {
+        background-image: url('./images/center_on.png');
+      }
+    }
+
+    &.z-act {
+      color: #ff5f16;
+    }
+  }
+}
 </style>
--- fore-end/src/views/Films.vue
+++ fore-end/src/views/Films.vue
@@ -31,10 +31,10 @@
     <!-- tab-bar -->
     <div class="tab-bar-wrapper">
       <ul class="tab-bar">
-        <li class="z-act">
+        <li :class="{'z-act': $route.path === '/films/nowPlaying'}" @click="switchList('now')">
           <span>正在热映</span>
         </li>
-        <li>
+        <li :class="{'z-act': $route.path === './films/comingSoon'}" @click="switchList('soon')">
           <span>即将上映</span>
         </li>
       </ul>
@@ -42,42 +42,13 @@
     <!-- /tab-bar -->
 
     <!-- list -->
-    <div class="film-list-content">
-      <ul>
-        <li
-          v-for="(item, index) in films"
-          :key="index">
-          <div class="img">
-            <img :src="item.poster" alt="">
-          </div>
-          <div class="info">
-            <div>
-              <span class="name">{{ item.name }}</span>
-              <span class="type">{{ item.filmType.name }}</span>
-            </div>
-            <div>
-              <span class="label">观众评分</span>
-              <span class="grade">{{ item.grade }}</span>
-            </div>
-            <div>
-              <span class="label">主演： {{ actorsList(item.actors) }}</span>
-            </div>
-            <div>
-              <span class="label">{{ item.nation }} | {{ item.runtime }}分钟</span>
-            </div>
-          </div>
-          <div class="buy">购票</div>
-        </li>
-      </ul>
-    </div>
+
     <!-- /list -->
   </div>
 </template>
-
 <script>
 // 引入 Swiper
 import Swiper from 'swiper';
-import axios from 'axios';
 
 export default {
   name: 'Films',
@@ -85,9 +56,7 @@ export default {
   data () {
     return {
       // 当前城市
-      curCity: '',
-
-      films: []
+      curCity: ''
     }
   },
 
@@ -104,40 +73,22 @@ export default {
     },
 
     /**
-     * 获取影片
+     * 切换路由
      */
-    getFilms () {
-      axios.get('/static/api/homepage.json')
-        .then((response) => {
-          // PS: res 不单单包含后台给的数据，还有一些个额外的东西。
-          // console.log(res);
-          let result = response.data;
-          console.log(result);
-          if (result.status === 0) {
-            this.films = result.data.films;
-          } else {
-            alert(result.msg);
-          }
+    switchList (type) {
+      if (type === 'now') {
+        // this.$router.push('/films/nowPlaying');
+        this.$router.push({
+          name: 'nowPlaying'
         })
-    },
-
-    /**
-     * 排列我们主演列表
-     * @param {Array} list 主演列表
-     */
-    actorsList (list) {
-      let arr = [];
-      arr = list.map(item => {
-        return item.name;
-      });
-
-      return arr.join(' ');
+      } else {
+        this.$router.push('/films/comingSoon');
+      }
     }
   },
 
   created () {
     this.getCityName();
-    this.getFilms();
   },
 
   mounted () {
@@ -156,126 +107,136 @@ export default {
 </script>
 
 <style lang="scss">
-/* @import '../styles/common/px2rem.scss'; */
-@import '@/styles/common/px2rem.scss';
-@import 'swiper/dist/css/swiper.min.css';
+  @import '@/styles/common/px2rem.scss';
+  @import 'swiper/dist/css/swiper.min.css';
 
-.films-list {
-  flex: 1;
-  overflow-y: auto;
-}
-
-.swiper-container {
-  height: px2rem(210);
-
-  .swiper-pagination-bullet {
-    width: px2rem(10);
-    height: px2rem(10);
+  .films-list {
+    flex: 1;
+    overflow-y: auto;
   }
-  .swiper-pagination-bullet-active {
-    background: yellowgreen;
-  }
-}
 
-.city-fixed {
-  position: absolute;
-  top: px2rem(18);
-  left: px2rem(8);
-  z-index: 999;
-  height: px2rem(30);
-  line-height: px2rem(30);
-  font-size: px2rem(14);
-  color: #fff;
-  border-radius: px2rem(15);
-  text-align: center;
-  padding: 0 px2rem(5);
-  background: rgba(0, 0, 0, 0.2);
-}
+  .swiper-container {
+    height: px2rem(210);
 
-.tab-bar-wrapper {
-  position: sticky;
-  z-index: 999;
-  top: px2rem(0);
-  height: px2rem(50);
-  border-bottom: px2rem(1) solid #ededed;
-  background: #fff;
-  .tab-bar {
-    display: flex;
-    height: px2rem(50);
-    align-items: center;
-    li {
-      flex: 1;
-      text-align: center;
-      font-size: px2rem(14);
-      span {
-        padding: 0 px2rem(10);
-      }
-      &.z-act {
-        color: #ff5f16;
-      }
+    .swiper-pagination-bullet {
+      width: px2rem(10);
+      height: px2rem(10);
     }
-  }
-}
 
-.film-list-content {
-  li {
-    display: flex;
-    margin: 0 px2rem(15);
-    padding: px2rem(15) 0;
-    border-bottom: px2rem(1) solid #ededed;
-  }
-
-  .img {
-    flex-shrink: 0;
-    width: px2rem(66);
-    height: px2rem(94);
-    img {
-      width: 100%;
+    .swiper-pagination-bullet-active {
+      background: yellowgreen;
     }
   }
 
-  .info {
-    min-width: 100px;
-    padding: 0 px2rem(10);
+  .city-fixed {
+    position: absolute;
+    top: px2rem(18);
+    left: px2rem(8);
+    z-index: 999;
+    height: px2rem(30);
+    line-height: px2rem(30);
     font-size: px2rem(14);
-    &>div {
-      width: 100%;
-      height: px2rem(22);
-      line-height: px2rem(22);
-      white-space: nowrap;
-      overflow: hidden;
-      text-overflow: ellipsis;
-    }
-    .label {
-      color: #797d82;
-      margin-right: px2rem(2);
-    }
-    .name {
-      font-size: px2rem(16);
-      color: #191a1b;
-    }
-    .type {
-      font-size: px2rem(12);
-      color: #fff;
-      background: #d2d6dc;
-      padding: 0 px2rem(2);
-    }
-    .grade {
-      color: #ffb232;
-    }
-  }
-
-  .buy {
-    flex-shrink: 0;
-    align-self: center;
-    width: px2rem(50);
-    height: px2rem(26);
-    line-height: px2rem(26);
-    font-size: px2rem(14);
-    color: #ff5f16;
-    border: px2rem(1) solid #ff5f16;
+    color: #fff;
+    border-radius: px2rem(15);
     text-align: center;
-    border-radius: px2rem(4);
+    padding: 0 px2rem(5);
+    background: rgba(0, 0, 0, 0.2);
   }
-}
+
+  .tab-bar-wrapper {
+    position: sticky;
+    z-index: 999;
+    top: px2rem(0);
+    height: px2rem(50);
+    border-bottom: px2rem(1) solid #ededed;
+    background: #fff;
+
+    .tab-bar {
+      display: flex;
+      height: px2rem(50);
+      align-items: center;
+
+      li {
+        flex: 1;
+        text-align: center;
+        font-size: px2rem(14);
+
+        span {
+          padding: 0 px2rem(10);
+        }
+
+        &.z-act {
+          color: #ff5f16;
+        }
+      }
+    }
+  }
+
+  .film-list-content {
+    li {
+      display: flex;
+      margin: 0 px2rem(15);
+      padding: px2rem(15) 0;
+      border-bottom: px2rem(1) solid #ededed;
+    }
+
+    .img {
+      flex-shrink: 0;
+      width: px2rem(66);
+      height: px2rem(94);
+
+      img {
+        width: 100%;
+      }
+    }
+
+    .info {
+      min-width: 100px;
+      padding: 0 px2rem(10);
+      font-size: px2rem(14);
+
+      &>div {
+        width: 100%;
+        height: px2rem(22);
+        line-height: px2rem(22);
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
+
+      .label {
+        color: #797d82;
+        margin-right: px2rem(2);
+      }
+
+      .name {
+        font-size: px2rem(16);
+        color: #191a1b;
+      }
+
+      .type {
+        font-size: px2rem(12);
+        color: #fff;
+        background: #d2d6dc;
+        padding: 0 px2rem(2);
+      }
+
+      .grade {
+        color: #ffb232;
+      }
+    }
+
+    .buy {
+      flex-shrink: 0;
+      align-self: center;
+      width: px2rem(50);
+      height: px2rem(26);
+      line-height: px2rem(26);
+      font-size: px2rem(14);
+      color: #ff5f16;
+      border: px2rem(1) solid #ff5f16;
+      text-align: center;
+      border-radius: px2rem(4);
+    }
+  }
 </style>
